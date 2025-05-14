33708347d6fe4469d777d7dfde47c64fc1914191
victorgeroto <victorgeroto28@gmail.com>
2025-05-14 17:42:12 -0300

    feat: adicionando tabela login

--- DDL.sql
+++ DDL.sql
@@ -39,3 +39,9 @@ CREATE TABLE deteccao_sensor (
     id_sensor INT REFERENCES sensor(id_sensor),
     timestamp_deteccao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
+
+CREATE TABLE login (
+    id SERIAL PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    senha VARCHAR(255) NOT NULL
+);
