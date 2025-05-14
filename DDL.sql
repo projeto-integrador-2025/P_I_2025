1b1c276fb7be1d4576942e5f4fd9885bbe6ae77d
victorgeroto <victorgeroto28@gmail.com>
2025-05-14 16:34:10 -0300

    feat:(books): adicionando o banco de dados

---
+++ DDL.sql
@@ -0,0 +1,41 @@
+----------------------------------DDL-----------------------------------------------
+
+CREATE TABLE estacao (
+    id_estacao SERIAL PRIMARY KEY,
+    descricao VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE estacao_estado (
+    id_estado SERIAL PRIMARY KEY,
+    id_estacao INT REFERENCES estacao(id_estacao),
+    estado BOOLEAN NOT NULL,
+    timestamp_estado TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE peca (
+    id_peca SERIAL PRIMARY KEY,
+    tipo_material VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE ciclo (
+    id_ciclo SERIAL PRIMARY KEY,
+    id_peca INT REFERENCES peca(id_peca),
+    id_estacao INT REFERENCES estacao(id_estacao),
+    tempo_inicial TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    timestamp_ciclo TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+
+CREATE TABLE sensor (
+    id_sensor SERIAL PRIMARY KEY,
+    tipo_sensor VARCHAR(50),
+    id_estacao INT REFERENCES estacao(id_estacao),
+    descricao VARCHAR(255)
+);
+
+
+CREATE TABLE deteccao_sensor (
+    id_deteccao SERIAL PRIMARY KEY,
+    id_sensor INT REFERENCES sensor(id_sensor),
+    timestamp_deteccao TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
