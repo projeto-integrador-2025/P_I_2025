4a674eacb5e8ed29beedf850dec0630e92b8f51d
05samuk <samuelribs1308@gmail.com>
2025-06-16 18:25:49 -0300

    feat: config of api

--- integracao/mobile/projetoSenai/services/api.ts
+++ integracao/mobile/projetoSenai/services/api.ts
@@ -1,9 +1,8 @@
 import axios from 'axios';
 
-// Coloque aqui o IP do seu computador (o mesmo que apareceu no ipconfig)
 const api = axios.create({
-  baseURL: 'http://10.109.25.40:5286/api', // <-- Troque se o IP mudar
-  timeout: 5000, // Tempo limite de resposta da API (5 segundos)
+  baseURL: 'http://54.85.232.244:5286/api',
+  timeout: 5000,
 });
 
 export default api;
